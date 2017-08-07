ee06402c95f0cdbf85651c7a28c9fa4c3172dd81
Connar Stone <tarstarkes@hotmail.com>
2017-08-07 11:30:51 -0700

    Updated for iOS submission
    updated 512px icon and 50px icon to config.xml. Also changed app to
    clear selections upon hitting "leg complete"

--- platforms/browser/platform_www/cordova_plugins.js
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -202,6 +202,20 @@ module.exports = [
         "clobbers": [
             "window.FileTransfer"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "pluginId": "cordova-plugin-statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/src/browser/StatusBarProxy.js",
+        "id": "cordova-plugin-statusbar.StatusBarProxy",
+        "pluginId": "cordova-plugin-statusbar",
+        "runs": true
     }
 ];
 module.exports.metadata = 
@@ -210,7 +224,8 @@ module.exports.metadata =
     "cordova-plugin-file": "4.3.3",
     "cordova-plugin-compat": "1.0.0",
     "cordova-plugin-file-transfer": "1.6.4-dev",
-    "cordova-plugin-whitelist": "1.3.2"
+    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-statusbar": "2.2.3"
 }
 // BOTTOM OF METADATA
 });
